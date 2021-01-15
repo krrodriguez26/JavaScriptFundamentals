7cd7c05155712a0900461e9f173781c9c1011b4d
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-15 01:12:54 -0500

    Added functions for buttons

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 var startButton = document.getElementById('start-btn')
 var questionContainerElement = document.getElementById('question-container')
-startButton.addEventListener('click', startGame)
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+var shuffledQuestions, currentQuestionIndex 
+
+
 
 function startQuiz() {
- console.log('Started')
+ console.log('Started')   
  startButton.classList.add('hide')
+ shuffledQuestions = questions.sort(() => Math.random() - .5)
+ currentQuestionIndex = 0
  questionContainerElement.classList.remove('hide')
+ startButton.addEventListener('click', startQuiz)
  setNextQuestion()
 }
 
-function setNextQuestion() {
 
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+    }
+
+function showQuestion(question) {
+ questionElement.innerText = question.question
+ question.answers.forEach(answer => {
+ var button = document.createElement('button')
+ button.innerText = answer.text
+ button.classList.add('btn')
+ if (answer.correct) {
+     button.dataset.correct = answer.correct
+ }
+ button.addEventListener('click', selectAnswer)
+ answerButtonsElement.appendChild(button)
+ })
 }
 
 function selectAnswer(){
 
 }
-
+ 
 var questions = [
     {
-        question: ' ',
+        question: 'Inside which HTML element do we put the JavaScript?',
         answers: [
-            { text: }
-            { text:}
+            { text: '<js>', correct: false },
+            { text: '<scripting>', correct: false },
+            { text: '<script>', correct: true }, 
+            { text: '<javascriptt>', correct: false }
         ]
     }
 ]

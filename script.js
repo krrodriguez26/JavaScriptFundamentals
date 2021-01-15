4158c705c5370c9af13b5f9bd0203bf28f23699a
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-15 01:33:07 -0500

    Added questions and answers for quiz

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var startButton = document.getElementById('start-btn')
+var  startButton = document.getElementById('start-btn')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
+
 var shuffledQuestions, currentQuestionIndex 
 
 
@@ -49,5 +50,55 @@ var questions = [
             { text: '<script>', correct: true }, 
             { text: '<javascriptt>', correct: false }
         ]
+        
+    }
+]
+
+var questions = [
+    {
+        question: 'Where is the correct place to insert a JavaScript?',
+        answers: [
+            { text: 'Both the <head> section and the <body> section are correct' , correct: true },
+            { text: 'The <body> section ', correct: false },
+            { text: 'The <head> section', correct: false }, 
+            { text: 'All of the above', correct: false }
+        ]
+        
+    }
+]
+
+var questions = [
+    {
+        question: 'Which event occurs when the user clicks on an HTML element?',
+        answers: [
+            { text: 'onclick' , correct: true },
+            { text: 'onmouseover', correct: false },
+            { text: 'onchange', correct: false }, 
+            { text: 'onmouseclick', correct: false }
+        ]
+        
+    }
+]
+
+var questions = [
+    {
+        question: 'The external JavaScript file must contain the <script> tag?',
+        answers: [
+            { text: 'True' , correct: false},
+            { text: 'False', correct: true },
+        ]
+        
+    }
+]
+
+var questions = [
+    {
+        question: 'How do you create a function in JavaScript?',
+        answers: [
+            { text: 'function myFunction()' , correct: true},
+            { text: 'function = myFunction()', correct: false},
+            { text: 'function:myFunction()', correct: false},
+        ]
+        
     }
 ]

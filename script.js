21105f20ee4a374a148b9ad35c16c013cd412877
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-18 18:15:54 -0500

    made edits to the css sheet to change collors when questions are answered

--- script.js
+++ script.js
@@ -14,38 +14,51 @@ let shuffledQuestions, currentQuestionIndex
         
     {
         question:"Inside which HTML element do we put the JavaScript?",
-             answers:["<js>", "<scripting>", "<script>", "<javascriptt>",],
-             // answer should have matching string from choices
-             correct:"<script>"
+             answers: [
+               { text: "<js>", correct: false},
+               { text: "<scripting>", correct: false},
+               { text: "<script>", correct: true},
+               { text: "<javascript>", correct: false}
+                ]
     },  
     {
         question:"Where is the correct place to insert a JavaScript?",
-        answers:["Both the <head> and the <body>", "The <body> section", "The <head> section","All of the above"],
-           // answer should have matching string from choices
-           correct:"Both the <head> and the <body>"      
+        answers: [
+            { text: "Both the <head> and the <body>",correct: true},
+            { text: "The <body> section", correct: false},
+            { text: "The <head> section", correct: false},
+            { text: "All of the above", correct: false}
+        ]    
     },
     {
         question: 'Which event occurs when the user clicks on an HTML element?',
-        answers:["onclick","onmouseover","onchange","onmouseclick",],
-         // answer should have matching string from choices
-         correct:"onclick"   
+        answers:[
+            { text: "onclick", correct: true },
+            { text: "onmouseover", correct: false},
+            { text: "onchange", correct: false},
+            { text: "onmouseclick",correct: false}  
+        ]   
      },
     {
         question: "JavaScript is the same as Java?",
-        answers:["True", "False"],
-         // answer should have matching string from choices
-         correct: "False"     
+        answers:[
+            {text: "True", correct: false},
+            {text: "False", correct: true}    
+        ]     
     },
     {
         question: 'How do you create a function in JavaScript?',
-        answers:["function:myFunction()", "function = myFunction()","function myFunction()", "All of the above"],
-         // answer should have matching string from choices
-        correct: "function myFunction()"   
-           
+        answers:[
+
+            {text: "function:myFunction()", correct: false},
+            {text: "function = myFunction()", correct: false},
+            {text: "function myFunction()", correct: true},
+            {text: "All of the above", correct: false},  
+        ]       
     }
 ];
 
-
+var score= 0;
 var questionIndex = 0;
 
 startButton.addEventListener("click", startQuiz);
@@ -63,10 +76,12 @@ nextButton.addEventListener("click", () => {
     setNextQuestion()
     renderCounter()
     TIMER = setInterval(renderCounter,1000)
+    let score = 0;
    }
 
  // Next question
     function setNextQuestion() {
+      resetState()
       showQuestion(shuffledQuestions[currentQuestionIndex])
     }
 
@@ -77,12 +92,13 @@ nextButton.addEventListener("click", () => {
            button.innerText = answer.text 
            button.classList.add("btn")
            if (answer.correct) {
-               button.daraset.correct = answer.correct
-           }
+               button.dataset.correct = answer.correct
+           } 
            button.addEventListener("click", selectAnswer) 
            answerButtonsElement.appendChild(button)
        })
     } 
+    
     function resetState(){
         nextButton.classList.add("hide")
         while (answerButtonsElement.firstChild) {
@@ -90,6 +106,7 @@ nextButton.addEventListener("click", () => {
             (answerButtonsElement.firstChild)
         }
     }
+
     function selectAnswer(e) {
         const selectedButton = e.target
         const correct = selectedButton.dataset.correct
@@ -97,14 +114,19 @@ nextButton.addEventListener("click", () => {
         Array.from(answerButtonsElement.children).forEach(button => {
             setStatusClass(button, button.dataset.correct)
    })
-        nextButton.classList.remove("hide")
-    }
-
+   if (shuffledQuestions.length > currentQuestionIndex + 1){
+       nextButton.classList.remove("hide")
+      } else {
+          startButton.innerText = "Restart"
+          startButton.classList.remove("hide")
+       }
+   }
+        
     function setStatusClass(element, correct){
        clearStatusClass(element)
        if (correct) {
-           element.classList.add("correct")
-       }else{
+           element.classList.add("correct") 
+       } else {
           element.classList.add("wrong") 
        }
     }
@@ -113,14 +135,24 @@ nextButton.addEventListener("click", () => {
          element.classList.remove("correct")
          element.classList.remove("wrong")
     }
-    
-    // Render questions and answers
-    function render(questionIndex){
-    for(let i=0; i< question.length; i++) {
-        var question = question[questionIndex].question
-        var answers = question[questionIndex].answers
-    }
 
+    //Check answer
+    function checkAnswer(answer){
+        if( answer == question [questionIndex].correct) {
+            // answer is correct
+          score++ 
+        } else {
+            // answer is wrong
+            // change button color to red
+        }
+    }
+     
+    // answer is correct
+    function answerIsCorrect(){
+        document.getElementById(questionIndex).style.backgroundColor ="#green";
+    }
+     
+ 
     // Counter render
      var count = 0;
      var questionTime = 10;
@@ -129,13 +161,20 @@ nextButton.addEventListener("click", () => {
      var Timer;
      var score = 0;
     
-
-      function renderCounter() {
+      function renderCounter(){
     if(count <= questionTime) {
         counter.innerHTML = count;
         timeGauge.style.width = count * gaugeUnit + "px";
         count++
     }else{
        count = 0;
+    }
+}
+
+// Render questions and answers
+function render(questionIndex){
+    for(let i=0; i< question.length; i++) {
+        var question = question[questionIndex].question
+        var answers = question[questionIndex].answers
     }
 }
